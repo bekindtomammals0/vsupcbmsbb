36f1d5e992ed8f4f9c1ce8982182d71a0abf6a38
Bµlawan <brianbulawan5@gmail.com>
2023-11-23 12:50:53 +0800

    Ep 31 Convert HTML and CSS to Blade
    Downloaded ZIP file from https://github.com/laracasts/Laravel-From-Scratch-HTML-CSS to use image resources and existing html and css code.
    
    Migrated html and css code to usable blade component.

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,6 +1,22 @@
-@extends('layout')
-@section('content')
-    <h2>You are now in Products Page</h2>
+<x-layout>
+    @include ('_products-header')'
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-featured-product-card/>
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-product-card/>
+            <x-product-card/>
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-product-card/>
+            <x-product-card/>
+            <x-product-card/>
+        </div>
+    </main>
+
+    {{-- <h2>You are now in Products Page</h2>
     @foreach($products as $product)
         <article class="{{$loop->odd?'tae':''}}">
             <a href="/products/{{$product->slug}}">
@@ -19,6 +35,5 @@
                 {!! $product->excerpt; !!}
             </div>
         </article>
-    @endforeach
-@endsection
-
+    @endforeach --}}
+</x-layout>
